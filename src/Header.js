506b01bef1da7a0d1c6a1631420c184c6186a5dc
shipwoli <khalwalesaviour@gmail.com>
2023-03-06 23:57:35 +0300

    added header

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="/">Memegenerator</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/memes">All Memes</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/mymemes">My Memes</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/addmeme">Add Meme</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
