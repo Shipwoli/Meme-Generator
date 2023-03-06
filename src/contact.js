8d130d23c9648e8fb96dfb7bd1535d0d6e704094
shipwoli <khalwalesaviour@gmail.com>
2023-03-06 23:54:03 +0300

    Updated with contact

---
+++ src/contact.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+      <p>If you have any questions or comments about the Memegenerator application, please feel free to contact us using the form below:</p>
+      <form>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" id="name" placeholder="Enter your name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter your email" />
+          <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea className="form-control" id="message" rows="3" placeholder="Enter your message"></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
